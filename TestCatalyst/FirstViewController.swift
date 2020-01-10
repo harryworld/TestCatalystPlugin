61883a19d5bd2af1bc7d4e1af5df78385c1097cc
Harry Ng <harryworld@gmail.com>
2020-01-10 11:15:07 +0800

    Initial Commit

---
+++ TestCatalyst/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TestCatalyst
+//
+//  Created by Harry Ng on 10/1/2020.
+//  Copyright © 2020 StaySorted Inc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TestCatalyst/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TestCatalyst
+//
+//  Created by Harry Ng on 10/1/2020.
+//  Copyright © 2020 StaySorted Inc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
